adf8d2e6a98652fcf1a9eab685e1335debb2a5a1
Sagar <>
2022-08-04 00:31:56 +0530

    adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import '../styles/globals.css'
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+  <>
+  <script 
+   async strategy="afterInteractive"
+   onError={ (e) => { console.error('Script failed to load', e) }}
+   src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7914014072217717"
+     ></script>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
