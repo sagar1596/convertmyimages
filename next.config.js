7661686c586e9cd1b1080b38bc7ae09c95b2ba63
Sagar <>
2022-08-25 16:37:33 +0530

    Changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  responseLimit: false,
-  compress: true
+  swcMinify: true
 }
 
 module.exports = nextConfig
--- pages/api/convert.js
+++ pages/api/convert.js
@@ -36,52 +36,11 @@ const post = async (req, res) => {
         break;
     }
 
+
+
     const bufferConverted = await img.getBufferAsync(formatMIME);
     const base64Data = bufferConverted.toString('base64');
     res.status(202).json({ b64Data: base64Data, contentType: targetFormat, extension:targetFormat.split('/')[1]});
-    // const cb = (bufferConverted) => {
-    //   if(bufferConverted) {
-    //     const base64Data = bufferConverted.toString('base64');
-    //     res.status(202).json({ b64Data: base64Data, contentType: targetFormat, extension:targetFormat.split('/')[1]});
-    //   }
-    // }
-    // img.getBuffer(formatMIME, cb);
-
-    // res.status(202).json({ "sagar": "awesome"});
-
-    // const form = new formidable.IncomingForm();
-    // form.parse(req, async (err, fields, files) => {
-
-    //   const targetFormat = fields.tf || "image/png";
-
-
-    //   // Read the image
-    //   const img = await jimp.read(files.file.filepath);
-
-    //   // Create a buffer based on required format
-    //   let formatMIME = jimp.MIME_PNG;
-    //   switch(targetFormat) {
-    //     case "image/png":
-    //       formatMIME = jimp.MIME_PNG;
-    //       break;
-    //     case "image/jpeg":
-    //     case "image/jpg":
-    //       formatMIME = jimp.MIME_JPEG;
-    //       break;
-    //     case "image/bmp":
-    //       formatMIME = jimp.MIME_BMP;
-    //       break;
-    //   }
-    //   const cb = (bufferConverted) => {
-    //     const base64Data = bufferConverted.toString('base64');
-    //     res.status(202).json({ b64Data: base64Data, contentType: targetFormat, extension:targetFormat.split('/')[1]});
-    //   }
-    //   // const bufferConverted = await img.getBufferAsync(formatMIME);
-    //   image.getBuffer(mime, cb);
-      
-
-      
-    // });
   } catch(err) {
     console.log(err);
     res.status(404).end();
@@ -90,17 +49,6 @@ const post = async (req, res) => {
   
 };
 
-const _convertToPng = (file) => {
-  
-}
-
-const saveFile = async (file) => {
-  const data = fs.readFileSync(file.filepath);
-  fs.writeFileSync(`./public/${file.originalFilename}`, data);
-  await fs.unlinkSync(file.filepath);
-  return;
-};
- 
 export default (req, res) => {
   req.method === "POST"
     ? post(req, res)
